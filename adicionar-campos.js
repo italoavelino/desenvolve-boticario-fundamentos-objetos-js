4c7de01b77eddd29e7a4764b22a27aaa6668935b
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-03-10 20:37:07 -0300

    Aprendendo a adicionar novas chaves no objeto

---
+++ adicionar-campos.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Ítalo",
+    idade: 21,
+    cpf: "123.456.789-10",
+    email: "italo@email.com"
+};
+
+console.log(cliente);
+
+cliente.fone = "912345678";
+
+console.log(cliente);
