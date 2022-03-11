3f685e5bd7e5d18e2bc696c5261bfdd6a97f323a
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-03-10 21:05:41 -0300

    Utilizando objetos dentro de objetos

---
+++ compondo-objetos.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Ítalo",
+    idade: 21,
+    cpf: "123.456.789-10",
+    email: "italo@email.com",
+    fones:["31912345678", "31998765432"]
+}; 
+
+cliente.dependentes = {
+    nome: "Sara",
+    parentesco: "filha",
+    dataNasc: "20/03/2011"
+};
+
+console.log(cliente);
