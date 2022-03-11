c3562b5c99b37a6b86cc29593c27f2e95b8b0b02
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-03-10 21:03:17 -0300

    Utilizando arrays dentro de objetos

---
+++ acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Ítalo",
+    idade: 21,
+    cpf: "123.456.789-10",
+    email: "italo@email.com",
+    fones:["31912345678", "31998765432"]
+}; 
+
+cliente.fones.forEach(fone => console.log(fone));
